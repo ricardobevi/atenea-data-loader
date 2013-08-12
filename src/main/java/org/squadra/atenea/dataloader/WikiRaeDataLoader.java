400aa35d490cd17d4a07ef2557cbda19b41abc45
facu <facu@localhost>
2013-08-11 23:47:50 -0300

    elimino dependencia de wordclasiffier

--- src/main/java/org/squadra/atenea/dataloader/WikiRaeDataLoader.java
+++ src/main/java/org/squadra/atenea/dataloader/WikiRaeDataLoader.java
@@ -3,15 +3,15 @@ package org.squadra.atenea.dataloader;
 import java.awt.List;
 import java.util.ArrayList;
 
-import org.squadra.atenea.wordclassifier.WordClassifier;
-import org.squadra.atenea.wordclassifier.WordModel.Word;
+//import org.squadra.atenea.wordclassifier.WordClassifier;
+//import org.squadra.atenea.wordclassifier.WordModel.Word;
 
 
 public class WikiRaeDataLoader implements DataLoaderInterface {
 
 	public void loadData(String source) {
 
-		WordClassifier WC = new WordClassifier();
+		//WordClassifier WC = new WordClassifier();
 		//List<Node> results = new List<Node>();
 
 		// consume base mysql
