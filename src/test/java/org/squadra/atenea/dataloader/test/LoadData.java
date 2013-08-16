9e775d8b76de9deb5256eb856bc3a4b860259297
ric <ric@localhost>
2013-08-15 21:27:57 -0300

    Agrego en WikiEnhancedBulkLoader.

--- src/test/java/org/squadra/atenea/dataloader/test/LoadData.java
+++ src/test/java/org/squadra/atenea/dataloader/test/LoadData.java
@@ -2,16 +2,19 @@ package org.squadra.atenea.dataloader.test;
 
 import org.squadra.atenea.dataloader.DataLoaderInterface;
 import org.squadra.atenea.dataloader.ManualDataLoader;
+import org.squadra.atenea.dataloader.WikipediaBulkLoader;
 
 public class LoadData {
 	
 	public static void main(String[] args) {
 		
-		DataLoaderInterface loder = new ManualDataLoader();
+		//DataLoaderInterface loder = new ManualDataLoader();
 		
-		loder.loadData("http://es.wikipedia.org/wiki/Carabanchel");
+		//loder.loadData("http://es.wikipedia.org/wiki/Carabanchel");
 		
 		//while(true);
+		
+		WikipediaBulkLoader.run();
 		
 		System.exit(0);
 		
