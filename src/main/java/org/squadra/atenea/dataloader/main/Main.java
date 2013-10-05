15e84ef99b7a3415ca1194ef30668319841b2ae7
lea <lea@localhost>
2013-10-05 01:35:33 -0300

    Modifico DialogLoader para que pase por la gramatica antes de cargar los datos. Agrego AteneaParser al pom.

--- src/main/java/org/squadra/atenea/dataloader/main/Main.java
+++ src/main/java/org/squadra/atenea/dataloader/main/Main.java
@@ -1,18 +1,24 @@
 package org.squadra.atenea.dataloader.main;
 
+import org.squadra.atenea.dataloader.DialogLoader;
+import org.squadra.atenea.dataloader.ManualDataLoader;
 import org.squadra.atenea.dataloader.WikiEnhancedBulkLoader;
 import org.squadra.atenea.dataloader.WikipediaBulkLoader;
 
 public class Main {
 
 	public static void main(String args[]) {
-		WikipediaBulkLoader.run();
+		//WikipediaBulkLoader.run();
 		//WikiEnhancedBulkLoader.run();
+		//ManualDataLoader mdl = new ManualDataLoader();
+		//mdl.loadData("");
+		
+		new DialogLoader().loadData("./AteneaDialogResponses.txt");
 	}
 	
 	
 	//Devuelve todo
-//	START n=node(*)    
+//	START n=node(*)
 //	MATCH (n)-[r]->(m)
 //  RETURN n.word as from, r.id as sentence, r.seq as `->`, m.word as to;
 	
